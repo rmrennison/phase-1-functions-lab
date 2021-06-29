d081a1d41c8e974a3238eb242c9f3bb99bd543b0
RyanRennison <rmrennison@gmail.com>
2021-06-29 19:14:57 -0400

    commit tree 6dd2af7a8ffe687fb94ede7fac51dab941265c40

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution in this file!
+// headquarters is on 42nd
+function distanceFromHqInBlocks(streetNumber) {
+    let result 
+    if (streetNumber < 42) {
+        result = 42 - streetNumber
+    } else {
+        result = streetNumber - 42
+    }
+
+    return result
+}
+// one block 264 ft
+function distanceFromHqInFeet(streetNo) {
+    const totalBlocks = distanceFromHqInBlocks(streetNo)
+    return totalBlocks * 264 
+}
